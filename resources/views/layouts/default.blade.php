4d269ce9edb88ca3db97a23b4c00e3ec352223f4
GuitarCYQ <1159543426@qq.com>
2020-09-27 09:44:08 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title','Weibo2 App')</title>
+    <title>@yield('title','Weibo2 App')  - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+                <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="content">
+        @yield('content')
+    </div>
+
 </body>
 </html>
