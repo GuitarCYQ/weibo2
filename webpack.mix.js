bd270fc4ad26d89d70a051b210f0475ea9a9631e
GuitarCYQ <1159543426@qq.com>
2020-09-28 01:58:52 +0000

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
+//version()解决浏览器缓存问题
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').version();
