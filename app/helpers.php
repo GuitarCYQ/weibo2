886e649551b7af8162ae14f48e1daf7a1b85749d
GuitarCYQ <1159543426@qq.com>
2020-10-06 01:58:20 +0000

    Updated database configuration

--- app/helpers.php
+++ app/helpers.php
@@ -1,17 +1,18 @@
 <?php
 
-function get_db_config(){
-    if (getenv('IS_IN_HEROKU')){
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
 
         return $db_config = [
-            'connection' => 'pasql',
-            'host'  =>  $url['host'],
-            'database'  =>  substr($url['path'], 1),
-            'username'  =>  $url['user'],
-            'password'  =>  $url['pass'],
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
         ];
-    }else{
+    } else {
         return $db_config = [
             'connection' => env('DB_CONNECTION', 'mysql'),
             'host' => env('DB_HOST', 'localhost'),
