6986caf3c8651d625cc9de07cae760e90744d965
GuitarCYQ <1159543426@qq.com>
2020-10-06 09:37:54 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -21,10 +21,18 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials)){
             session()->flash('success','欢迎回来!');
-            return redirect()->route('user.show',[Auth::user()]);
+            return redirect()->route('users.show',[Auth::user()]);
         }else{
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','您已成功退出');
+        return redirect('login');
+    }
+
 }
