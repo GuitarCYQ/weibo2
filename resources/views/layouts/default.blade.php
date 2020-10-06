ca3e1c0c5c5010cbd298ba2b6175432f301f62c8
GuitarCYQ <1159543426@qq.com>
2020-10-06 08:53:47 +0000

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,24 +1,21 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title','Weibo2 App')  - Laravel 入门教程</title>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
+
 <body>
+@include('layouts._header')
 
-    @include('layouts._header')
-
-    <div class="content">
-        <div class="offset-md-1 col-md-10">
-            @include('shared._messages')
-            @yield('content')
-            @include('layouts._footer')
-        </div>
+<div class="container">
+    <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
     </div>
+</div>
 
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
