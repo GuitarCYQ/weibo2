01e4d4e8bf090de8a97d9af077bbbfe2632fd398
GuitarCYQ <1159543426@qq.com>
2020-10-07 15:24:39 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,6 +21,12 @@ class UsersController extends Controller
         ]);
     }
 
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
     public function create()
     {
         return view('users.create');
