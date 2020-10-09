01c938a7927f63b7f8ccf8f33f04be522981f6da
GuitarCYQ <1159543426@qq.com>
2020-10-09 07:04:10 +0000

    关注按钮

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -30,4 +30,9 @@ class UserPolicy
     {
         return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
+
+    public function follow(User $currentUser, User $user)
+    {
+      return $currentUser->id !== $user->id;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,7 @@ Route::resource('statuses','StatusesController', ['only' => ['store','destroy']]
 Route::get('/users/{user}/followings','UsersController@followings')->name('users.followings');
 //显示用户的粉丝列表
 Route::get('/users/{user}/followers','UsersController@followers')->name('users.followers');
+
+//关注和取消关注
+Route::post('/users/followers/{user}','FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}','FollowersController@destroy')->name('followers.destroy');
