a1340845453019aeee7ba36fe6ee413f5eb7eb9b
GuitarCYQ <1159543426@qq.com>
2020-10-09 07:14:15 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,8 +61,11 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at','desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids,$this->id);
+        return Status::whereIn('user_id',$user_ids)
+            ->with('user')
+            ->orderBy('created_at','desc');
     }
 
     //获取粉丝列表
