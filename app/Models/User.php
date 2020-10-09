67c93a222a1fc20a3766d60bd98da2ba6c433e67
GuitarCYQ <1159543426@qq.com>
2020-10-09 03:49:04 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,43 @@ class User extends Authenticatable
         return $this->statuses()
                     ->orderBy('created_at','desc');
     }
+
+    //获取粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers' ,'user_id' ,'follower_id');
+    }
+
+    //获取关注列表
+    public function followings()
+    {
+        return  $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    //关注
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        //sync()第二个参数 移除重复的
+        $this->followings()->sync($user_ids, false);
+    }
+
+    //取消关注
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        //detach()用于取消关注
+        $this->followings()->detach($user_ids);
+    }
+
+    //判断是否关注
+    public function isFollowing($user_id)
+    {
+        //contains()判断是否关注
+        return $this->followings->contains($user_id);
+    }
 }
